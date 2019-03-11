213021000219c6a4b41d6df38f7369af70584b37
knee-cola <nikola.derezic@gmail.com>
2019-03-11 10:47:27 +0100

    Updates

---
+++ precache-manifest.687975f9580d7483b9caf511834d9e77.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "cad2dcbdff53a0796980d5d8ce2b3db8",
+    "url": "/portal-penjanje/static/media/velebitas.cad2dcbd.jpg"
+  },
+  {
+    "revision": "423a722ce211fbdb43212e0e0607cffc",
+    "url": "/portal-penjanje/static/media/grapa.423a722c.jpg"
+  },
+  {
+    "revision": "2ecf79ffaf5122d7194b",
+    "url": "/portal-penjanje/static/js/runtime~main.182d5f93.js"
+  },
+  {
+    "revision": "b293cc8d6e5279ccbab0",
+    "url": "/portal-penjanje/static/js/main.c63851fe.chunk.js"
+  },
+  {
+    "revision": "86f664ef17c1df319cf6",
+    "url": "/portal-penjanje/static/js/2.d6615bb7.chunk.js"
+  },
+  {
+    "revision": "b293cc8d6e5279ccbab0",
+    "url": "/portal-penjanje/static/css/main.8ff1ee0b.chunk.css"
+  },
+  {
+    "revision": "cb22bfb77773b512f3df062a7e452aa4",
+    "url": "/portal-penjanje/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6a2cf9f721fd943d0f7e87b6aef85949.js"
+  "/portal-penjanje/precache-manifest.687975f9580d7483b9caf511834d9e77.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/portal-penjanje/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
