7e33abffb331f7876e19c8ad38b80a84189f2998
knee-cola <nikola.derezic@gmail.com>
2019-03-11 10:41:02 +0100

    Updates

---
+++ precache-manifest.6a2cf9f721fd943d0f7e87b6aef85949.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "cad2dcbdff53a0796980d5d8ce2b3db8",
+    "url": "/static/media/velebitas.cad2dcbd.jpg"
+  },
+  {
+    "revision": "423a722ce211fbdb43212e0e0607cffc",
+    "url": "/static/media/grapa.423a722c.jpg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "50b99a48c400fc37c024",
+    "url": "/static/js/main.8872152f.chunk.js"
+  },
+  {
+    "revision": "86f664ef17c1df319cf6",
+    "url": "/static/js/2.d6615bb7.chunk.js"
+  },
+  {
+    "revision": "50b99a48c400fc37c024",
+    "url": "/static/css/main.8ff1ee0b.chunk.css"
+  },
+  {
+    "revision": "ded806680c525ef9944b3670ff06b826",
+    "url": "/index.html"
+  }
+];
