ca9c35ecbf5baed8001a6936a1c02967c6770206
knee-cola <nikola.derezic@gmail.com>
2019-03-11 10:16:46 +0100

    dovršen tutorial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import Button from '@material-ui/core/Button';
+import Bookmarks from '@material-ui/icons/Bookmarks';
+import NavBar from './components/NavBar';
+import { TextField } from '@material-ui/core';
+import MediaCard from './components/Card';
+import IMG1 from './img/velebitas.jpg';
+import IMG2 from './img/grapa.jpg';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <NavBar />
+        <Button variant="outlined" color="primary">
+          <Bookmarks></Bookmarks>
+        </Button> <br />
+        <TextField placeholder="Placeholder here" label="Basic TextField" />
+        <MediaCard uspon={{
+            penjaci:['Dragutin Vdović', 'Nikola Derežić'],
+            img: IMG1,
+            datumUspona:'23.03.2019.',
+            smjer:{
+                imeSmjera:'Velebitaš',
+                lokacijaSmjera:'Sjeverna stijena, Anića kuk, Velika Paklenica, Croatia',
+                ocjenaSmjera:'6a+'
+              }
+            }} />
+        <MediaCard uspon={{
+            penjaci:['Rene Lisac', 'Marin Šapit'],
+            img: IMG2,
+            datumUspona:'23.03.2019.',
+            smjer:{
+                imeSmjera:'Pripravniška grapa',
+                ocjenaSmjera:'VI+ / M3',
+                lokacijaSmjera:'Mala Mojstrovka, Julijske alpe, Slovenija',
+              }
+            }} />
       </div>
     );
   }
---
+++ src/components/Card.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+const styles = {
+  card: {
+    maxWidth: 345,
+    margin: '1em',
+  },
+  media: {
+    height: 140,
+  },
+};
+
+function MediaCard(props) {
+  const { classes, uspon: { penjaci, img, datumUspona, smjer: { imeSmjera, lokacijaSmjera, ocjenaSmjera } } } = props;
+  return (
+    <Card className={classes.card}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image= {img}
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+        <Typography gutterBottom variant="h5" component="h2">
+            {imeSmjera},{ocjenaSmjera}
+        </Typography>
+          <Typography component="i">
+            {lokacijaSmjera}
+          </Typography>
+          <Typography component="p">
+          {datumUspona} penjali {penjaci.join(', ')}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Detalji uspona
+        </Button>
+        <Button size="small" color="primary">
+          Želim to penjati
+        </Button>
+      </CardActions>
+    </Card>
+  );
+}
+
+MediaCard.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(MediaCard);
