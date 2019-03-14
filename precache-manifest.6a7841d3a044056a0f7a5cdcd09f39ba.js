4c79400c0fc8c80353bc22636fdc96bd4316d99b
knee-cola <nikola.derezic@gmail.com>
2019-03-14 08:08:09 +0100

    Updates

--- precache-manifest.6a7841d3a044056a0f7a5cdcd09f39ba.js
+++ precache-manifest.6a7841d3a044056a0f7a5cdcd09f39ba.js
@@ -12,19 +12,19 @@ self.__precacheManifest = [
     "url": "/portal-penjanje/static/js/runtime~main.182d5f93.js"
   },
   {
-    "revision": "13602941f8b612e43cc1",
-    "url": "/portal-penjanje/static/js/main.bd61dfb1.chunk.js"
+    "revision": "ce0bd5498e0ed538d01b",
+    "url": "/portal-penjanje/static/js/main.0aac0346.chunk.js"
   },
   {
-    "revision": "8230a3624fa5042c3485",
-    "url": "/portal-penjanje/static/js/2.ed18e444.chunk.js"
+    "revision": "c3bdbdbcf6b26bf8487b",
+    "url": "/portal-penjanje/static/js/2.760ded9d.chunk.js"
   },
   {
-    "revision": "13602941f8b612e43cc1",
+    "revision": "ce0bd5498e0ed538d01b",
     "url": "/portal-penjanje/static/css/main.8ff1ee0b.chunk.css"
   },
   {
-    "revision": "c00dbabf583dc9f432c3669d2727d9ef",
+    "revision": "9dbe5099bac57c484d373f1df7d1e150",
     "url": "/portal-penjanje/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portal-penjanje/precache-manifest.97512d9ca87604e3160cff5fed19e74f.js"
+  "/portal-penjanje/precache-manifest.6a7841d3a044056a0f7a5cdcd09f39ba.js"
 );
 
 workbox.clientsClaim();
