d9c6354d19422fb40983be3d8af28824dff11d87
Nikola Derežić <nikola.derezic@gmail.com>
2019-03-15 15:37:30 +0100

    dodana stranica "moji partneri"

---
+++ src/activities/MojiPartneri.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PartnerCard from '../components/cards/PartnerCard';
+import JimboAvatar from '../img/partneri/jimbo.jpg';
+import KrckoAvatar from '../img/partneri/krcko.jpg';
+import SandaAvatar from '../img/partneri/sanda.jpg';
+
+const partneri = [
+    { id: 0, ime: 'Dragutin', prezime: 'Vdović', nick:'krcko', avatar: KrckoAvatar },
+    { id: 1, ime: 'Dražen', prezime:'Đimoti', nick:'jimbo', avatar: JimboAvatar },
+    { id: 2, ime: 'Sanda', prezime:'Banović', nick:'banovec', avatar: SandaAvatar },
+    { id: 3, ime: 'Ivan', prezime:'Padavić', nick:'pado', avatar: null }
+];
+
+export const MojiPartneri = () => partneri.map(partner => <PartnerCard partner={partner} key={partner.id} />);
---
+++ src/components/cards/PartnerCard.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import defaultAvatar from '../../img/baseline-person-24px.svg';
+
+const styles = {
+  card: {
+    maxWidth: 345,
+    margin: '1em',
+  },
+  media: {
+    float: 'left',
+    height: 90,
+    width: 90,
+    marginRight: 10
+  },
+};
+
+function PartnerCard(props) {
+  
+  const { classes, partner: { ime, prezime, nick, avatar } } = props;
+
+  return (
+    <Card className={classes.card}>
+      <CardActionArea>
+        <CardMedia
+            className={classes.media}
+            image={avatar || defaultAvatar}
+            />
+        <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+                {ime} {prezime}
+            </Typography>
+            <Typography component="i">
+                @{nick}
+            </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+          <Button size="small" color="primary">
+          Zajedno smo penjali
+        </Button>
+        <Button size="small" color="primary">
+          Zadnje je penjao/la
+        </Button>
+      </CardActions>
+    </Card>
+  );
+}
+
+
+export default withStyles(styles)(PartnerCard);
