c31729506edc130a7b8f6724a12e7051531d881c
knee-cola <nikola.derezic@gmail.com>
2019-03-16 16:12:25 +0100

    UsponCard: dodan popup meni

--- src/cards/UsponCard.js
+++ src/cards/UsponCard.js
@@ -4,7 +4,7 @@ import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
-import { Link, CardHeader, IconButton, Icon, Collapse, CardActionArea } from '@material-ui/core';
+import { Link, CardHeader, IconButton, Icon, Collapse, CardActionArea, Menu, MenuItem } from '@material-ui/core';
 import { Link as RouterLink } from 'react-router-dom';
 import ImageGallery from '../components/ImageGallery';
 import classnames from 'classnames';
@@ -37,49 +37,63 @@ const styles = {
 function UsponCard(props) {
   
   const { classes, uspon: { penjaci, titleImage, images, datumUspona, napomena, smjer: { imeSmjera, lokacijaSmjera, ocjenaSmjera } } } = props;
-
   const [expanded, setExpanded] = React.useState(false);
+  const [menuAnchorEl, setMenuAnchorEl] = React.useState(null);
 
   function handleExpandClick() {
     setExpanded(!expanded);
   }
 
+  function handleClick(event) {
+    setMenuAnchorEl(event.currentTarget);
+  }
+
+  function handleClose() {
+    setMenuAnchorEl(null);
+  }
+
   return (
-    <Card className={classes.card}>
-      <CardHeader
-        action={
-          <IconButton>
-            <Icon>more_vert</Icon>
-          </IconButton>
-        }
-        title={`${imeSmjera}, ${ocjenaSmjera}`}
-        subheader={lokacijaSmjera}
-      />
-      <CardMedia
-        onClick={handleExpandClick}
-        className={classes.media}
-        image={titleImage}
-      />
-      <CardContent>
-        <Typography component="p">
-          {datumUspona} 
-        </Typography>
-        <Typography component="p">
-          penjali {penjaci.map(({nick, ime, prezime}, ix, self)=> <Fragment key={nick}>{ix===0 ? '': (ix === self.length-1 ? ' i ' : ', ')}<Link component={RouterLink} to={`@${nick}`}>{ime} {prezime}</Link></Fragment>)} 
-        </Typography>
-      </CardContent>
-      <CardActionArea onClick={handleExpandClick} aria-label="Show more" className={classes.actionArea}>
-        <Icon className={classnames(classes.expand, { [classes.expandOpen]: expanded })}>expand_more</Icon>
-      </CardActionArea>
-      <Collapse in={expanded} timeout="auto" unmountOnExit>
+    <Fragment>
+      <Card className={classes.card}>
+        <CardHeader
+          action={
+            <IconButton onClick={handleClick}>
+              <Icon>more_vert</Icon>
+            </IconButton>
+          }
+          title={`${imeSmjera}, ${ocjenaSmjera}`}
+          subheader={lokacijaSmjera}
+        />
+        <CardMedia
+          onClick={handleExpandClick}
+          className={classes.media}
+          image={titleImage}
+        />
         <CardContent>
-          <Typography component="p" className={classes.notes}>
-            {napomena}
+          <Typography component="p">
+            {datumUspona} 
           </Typography>
-          <ImageGallery images={images} />
+          <Typography component="p">
+            penjali {penjaci.map(({nick, ime, prezime}, ix, self)=> <Fragment key={nick}>{ix===0 ? '': (ix === self.length-1 ? ' i ' : ', ')}<Link component={RouterLink} to={`@${nick}`}>{ime} {prezime}</Link></Fragment>)} 
+          </Typography>
         </CardContent>
-      </Collapse>
-    </Card>
+        <CardActionArea onClick={handleExpandClick} aria-label="Show more" className={classes.actionArea}>
+          <Icon className={classnames(classes.expand, { [classes.expandOpen]: expanded })}>expand_more</Icon>
+        </CardActionArea>
+        <Collapse in={expanded} timeout="auto" unmountOnExit>
+          <CardContent>
+            <Typography component="p" className={classes.notes}>
+              {napomena}
+            </Typography>
+            <ImageGallery images={images} />
+          </CardContent>
+        </Collapse>
+      </Card>
+      <Menu id="simple-menu" anchorEl={menuAnchorEl} open={Boolean(menuAnchorEl)} onClose={handleClose}>
+        <MenuItem onClick={handleClose}>Info o smjeru</MenuItem>
+        <MenuItem onClick={handleClose}>Izmjeni zapis</MenuItem>
+      </Menu>
+    </Fragment>
   );
 }
 
