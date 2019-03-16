bf39fedbbb47514569cdd426f9505c87d5ace518
knee-cola <nikola.derezic@gmail.com>
2019-03-16 18:38:23 +0100

    izgled kartice "smjer" usklađena sa izgledom "uspon"

--- src/cards/SmjerCard.js
+++ src/cards/SmjerCard.js
@@ -1,50 +1,71 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import { Link, CardHeader, IconButton, Icon, Collapse, CardActionArea, Menu, MenuItem } from '@material-ui/core';
+import { Link as RouterLink } from 'react-router-dom';
+import ImageGallery from '../components/ImageGallery';
+import classnames from 'classnames';
 
 const styles = {
   card: {
-    maxWidth: 345,
-    margin: '1em',
+    maxWidth: 400,
+    margin: '1em'
   },
   media: {
-    height: 140,
+    borderTop: '1px solid #cccccc',
+    height: 0,
+    paddingTop: '56.25%', // 16:9
+    cursor: 'pointer'
   },
 };
 
 function SmjerCard(props) {
   
-  const { classes, smjer:{imeSmjera, thumb, lokacijaSmjera, ocjenaSmjera} } = props;
+  const { classes, smjer:{ imeSmjera, ocjenaSmjera, lokacijaSmjera, thumb } } = props;
+
+  const [expanded, setExpanded] = React.useState(false);
+  const [menuAnchorEl, setMenuAnchorEl] = React.useState(null);
+
+  function handleExpandClick() {
+    setExpanded(!expanded);
+  }
+
+  function handleClick(event) {
+    setMenuAnchorEl(event.currentTarget);
+  }
+
+  function handleClose() {
+    setMenuAnchorEl(null);
+  }
 
   return (
-    <Card className={classes.card}>
-      <CardActionArea>
-        <CardMedia
-          className={classes.media}
-          image= {thumb}
+    <Fragment>
+      <Card className={classes.card}>
+        <CardHeader
+          action={
+            <IconButton onClick={handleClick}>
+              <Icon>more_vert</Icon>
+            </IconButton>
+          }
+          title={`${imeSmjera}, ${ocjenaSmjera}`}
+          subheader={lokacijaSmjera}
         />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-              {imeSmjera}, {ocjenaSmjera}
-          </Typography>
-          <Typography component="i">
-            {lokacijaSmjera}
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-        <Button size="small" color="primary">
-          Info o smjeru
-        </Button>
-      </CardActions>
-    </Card>
+        <CardMedia
+          onClick={handleExpandClick}
+          className={classes.media}
+          image={thumb}
+        />
+      </Card>
+      <Menu id="simple-menu" anchorEl={menuAnchorEl} open={Boolean(menuAnchorEl)} onClose={handleClose}>
+        <MenuItem onClick={handleClose}>Info o smjeru</MenuItem>
+        <MenuItem onClick={handleClose}>Zabilježi uspon</MenuItem>
+      </Menu>
+    </Fragment>
   );
 }
 
-export default withStyles(styles)(SmjerCard);
+
+export default withStyles(styles)( SmjerCard ) ;
